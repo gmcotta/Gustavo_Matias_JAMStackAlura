3dacf8531ba35613b89e18f68dead43986ee07c3
Gustavo Cotta <gmcotta34@gmail.com>
2021-02-16 20:05:07 -0300

    Adding styled-normalized

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize};
+  
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +26,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
